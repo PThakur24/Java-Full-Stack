07cf94646942bcebc6bafda03d0a507251ba626f
PThakur24 <80519273+PThakur24@users.noreply.github.com>
2023-07-02 22:10:39 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class DataTypes {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		String y=sc.nextLine();
+		double z=sc.nextDouble();
+		
+		System.out.println("x,y,z are"+x+" "+y+" "+z);
+		}
+
+}
